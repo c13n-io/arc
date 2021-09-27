8e503ad9aee75b8e50b138ad05a088153b2b7b6a
Themis Vasiliou <tvasileiou@programize.com>
2021-09-27 10:54:28 +0300

    Changing the login page

--- src/components/accounts.js
+++ src/components/accounts.js
@@ -9,6 +9,7 @@ import { version } from "../config/version";
 import { addToAccounts, removeFromAccounts } from "../utils/accounts-utils";
 
 import "./accounts.css";
+import c13nLogo from "../media/C13N_Logo.png";
 
 const { Version } = require("../rpc/rpc_pb.js");
 
@@ -18,39 +19,15 @@ const { Version } = require("../rpc/rpc_pb.js");
  */
 const Accounts = (props) => {
   // Modal related variables
-  const [
-    addAccountModalVisible,
-    setAddAccountModalVisible
-  ] = useState(false);
-  const [
-    deleteAccountModalVisible,
-    setDeleteAccountModalVisible
-  ] =
+  const [addAccountModalVisible, setAddAccountModalVisible] = useState(false);
+  const [deleteAccountModalVisible, setDeleteAccountModalVisible] =
     useState(false);
-  const [
-    editAccountModalVisible,
-    setEditAccountModalVisible
-  ] = useState(false);
-  const [
-    credentialsModalVisible,
-    setCredentialsModalVisible
-  ] = useState(false);
-  const [
-    usernameToUse,
-    setUsernameToUse
-  ] = useState("");
-  const [
-    passwordToUse,
-    setPasswordToUse
-  ] = useState("");
-  const [
-    urlToAdd,
-    setUrlToAdd
-  ] = useState("");
-  const [
-    selectedAccount,
-    setSelectedAccount
-  ] = useState();
+  const [editAccountModalVisible, setEditAccountModalVisible] = useState(false);
+  const [credentialsModalVisible, setCredentialsModalVisible] = useState(false);
+  const [usernameToUse, setUsernameToUse] = useState("");
+  const [passwordToUse, setPasswordToUse] = useState("");
+  const [urlToAdd, setUrlToAdd] = useState("");
+  const [selectedAccount, setSelectedAccount] = useState();
 
   /**
    * This function is called once when the component loads.
@@ -125,10 +102,13 @@ const Accounts = (props) => {
    * The accounts page JSX.
    */
   return (
-    <div className="accountsScreen">
-      <h3>Select an account</h3>
+    <div className="login-page">
+      <div className="login-page-logo-container">
+        <img src={c13nLogo} className="login-page-logo" />
+      </div>
+      <h3 className="login-page-select">Select a c13n Node</h3>
       <List
-        className="accountsList"
+        className="login-page-accountsList"
         locale={{
           emptyText: "No Accounts",
         }}
@@ -137,20 +117,27 @@ const Accounts = (props) => {
           return (
             <List.Item className="accountsListItem">
               <Form className="accountsForm">
-                <Form.Item label="URL" className="accountsFormItem">
-                  {item.url !== "" ?
+                <Form.Item
+                  label="URL"
+                  className="accountsFormItem"
+                  onClick={() => {
+                    if (item.url !== "") {
+                      setSelectedAccount(item);
+                      setCredentialsModalVisible(true);
+                    }
+                  }}
+                >
+                  {item.url !== "" ? (
                     item.url
-                    :
+                  ) : (
                     <span className="accountsFormItemError">
                       No URL provided
                     </span>
-                  }
+                  )}
                 </Form.Item>
               </Form>
-              <br />
-              <br />
-              <div className="accountsDiv">
-                <Button
+              <div className="accountsButtons">
+                {/* <Button
                   className="accountsButton"
                   onClick={() => {
                     if (item.url !== "") {
@@ -161,19 +148,6 @@ const Accounts = (props) => {
                 >
                   Connect
                   <LoginOutlined />
-                </Button>
-                {/* <Button
-                  style={{
-                    display: 'inline-block',
-                    float: 'right'
-                  }}
-                  onClick={() => {
-                    setSelectedAccount(item)
-                    setUrlToAdd(item.url)
-                    setEditAccountModalVisible(true)
-                  }}
-                >
-                  <EditOutlined />
                 </Button> */}
                 <Button
                   className="accountsButton"
@@ -190,21 +164,21 @@ const Accounts = (props) => {
         }}
       />
       <Button
-        className="accountsButtonModalTrue"
+        className="login-page-addButton"
         onClick={() => {
           setAddAccountModalVisible(true);
         }}
       >
-        Add New Account
+        <div className="login-page-addButton-Text">+ Add New Node</div>
       </Button>
       <Modal
         visible={!!addAccountModalVisible}
-        title="Adding new account"
         onOk={() => {
           if (urlToAdd !== "") {
             addToAccounts(props, { url: urlToAdd });
             setUrlToAdd("");
             setAddAccountModalVisible(false);
+            credentialsModalLogin();
           }
         }}
         onCancel={() => {
@@ -212,12 +186,39 @@ const Accounts = (props) => {
         }}
         okButtonProps={{ type: "default" }}
       >
+        <div className="login-page-modal-logo-container">
+          <img src={c13nLogo} className="login-page-modal-logo" />
+        </div>
         <Input
-          placeholder="Public IP Address"
+          placeholder="Node Address"
           value={urlToAdd}
           className="accountsInput"
           onChange={(e) => {
             setUrlToAdd(e.target.value);
+          }}
+        />
+        <Input
+          placeholder="Username"
+          className="accountsInputCred"
+          value={usernameToUse}
+          onChange={(e) => {
+            setUsernameToUse(e.target.value);
+          }}
+        />
+        <Input
+          placeholder="Password"
+          className="accountsInputCred"
+          type="password"
+          value={passwordToUse}
+          onChange={(e) => {
+            setPasswordToUse(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            return e.key === "Enter"
+              ? e.shiftKey
+                ? undefined
+                : credentialsModalLogin()
+              : undefined;
           }}
         />
       </Modal>
@@ -245,6 +246,9 @@ const Accounts = (props) => {
         okButtonProps={{ type: "default" }}
         cancelButtonProps={{ style: { display: "none" } }}
       >
+        <div className="login-page-modal-logo-container">
+          <img src={c13nLogo} className="login-page-modal-logo" />
+        </div>
         <Input
           placeholder="Username"
           className="accountsInputCred"
