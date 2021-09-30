8c716b125a811842e941b80eae593acace504e07
George Tsagkarelis <georgets98@gmail.com>
2021-09-30 17:06:20 +0300

    Change app version to 0.0.2

--- src/config/version.js
+++ src/config/version.js
@@ -2,7 +2,7 @@ import nodeInfoClient from "../services/nodeInfoServices";
 /**
  * The FE application version.
  */
-const version = "0.0.1";
+const version = "0.0.2";
 
 /**
  * Acquires the returned backend version.
